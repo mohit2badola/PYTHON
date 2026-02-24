6bb7d1f33338b49eefaffa0c5a66b669dc61260c
mohit2badola <badolamohit52@gmail.com>
2026-02-24 17:48:04 +0530

    adding files

---
+++ test2.py
@@ -0,0 +1,45 @@
+# class Student:
+#     def set_name(self, name):
+#         self.name = name
+#         return self.name
+
+# s=Student()
+# arr=[]
+# n=True
+# while n:
+#     print("Enter 0 to exit")
+#     ch=input("Enter the Choice")
+#     if ch=="0":
+#         n=False
+#     else:
+#         x=input("Enter the name of the student")
+#         arr.append(s.set_name(x))
+    
+
+
+# print(arr)
+
+# ""Question 2""
+# class Counter:
+    
+#     def __init__(self):
+#         self.value = 0
+
+#     def inc(self):
+#         self.value += 1
+
+#     def dec(self):
+#         self.value -= 1
+
+#     def show(self):
+#         return self.value
+# c=Counter()
+# c.inc()
+# c.inc()
+# c.dec()
+# c.dec()
+# c.dec()
+# c.dec()
+# c.dec()
+# c.dec()
+# print(c.show())
